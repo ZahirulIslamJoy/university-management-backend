a895d6f63b085561f7554d087aaf83b5f5173f1b
ZahirulIslamJoy <neloy1613@gmail.com>
2024-07-31 13:44:25 +0600

    fix functional error in generating semesterid

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -9,12 +9,24 @@ const findLastSemesterId=async()=>{
         _id:0
     }).sort({createdAt:-1}).lean()
 
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined
+    //return lastStudent?.id ? lastStudent.id.substring(6) : undefined
+    return lastStudent?.id ? lastStudent.id : undefined
 }
 
 
 export const generateStudentId=async(payload: TAcademicSemester)=>{
-    const currentId= await  findLastSemesterId() || (0).toString();
+    let currentId=  (0).toString();
+
+    const laststudentId= await findLastSemesterId();
+    const lastStudentYear=laststudentId?.substring(0,4);
+    const lastStudentSemesterCode=laststudentId?.substring(4,6);
+    const currentStudentYear=payload.year;
+    const currentStudentSemesterCode=payload.code;
+
+    if(laststudentId && lastStudentSemesterCode === currentStudentSemesterCode && currentStudentYear ===  lastStudentYear){
+            currentId=laststudentId.substring(6)
+    }
+
     let incremrntId=(Number(currentId)+1).toString().padStart(4,"0");
     incremrntId=`${payload.year}${payload.code}${incremrntId}`
     return  incremrntId
