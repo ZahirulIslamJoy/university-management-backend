c1df688d1b84e60e7fae0a700cc140013e115fab
ZahirulIslamJoy <neloy1613@gmail.com>
2024-08-01 11:05:15 +0600

    apply transaction and rollback in delete user

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -1,28 +1,61 @@
+import { startSession } from 'mongoose';
 import { StudentModel } from './student.models';
-const getAllStudentsFromDB=async()=>{
-    const result=await StudentModel.find().populate("admissionSemester").populate({
-        path:"academicDepartment",
-        populate:{
-            path:"academicFaculty"
-        }
+import AppError from '../../app/errors/AppError';
+import httpStatus from 'http-status';
+import { User } from '../user/user.model';
+const getAllStudentsFromDB = async () => {
+  const result = await StudentModel.find()
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
     });
+  return result;
+};
+
+const getSingleStudentFromDB = async (id: string) => {
+  const result = await StudentModel.findById(id);
+  // const result=await StudentModel.aggregate([
+  //     {$match:{id:id}}
+  // ])
+  return result;
+};
+
+const deleteStudentFromDB = async (id: string) => {
+  const sesssion = await startSession();
+  try {
+    sesssion.startTransaction();
+    //transaction-1
+    const result = await StudentModel.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, sesssion },
+    );
+    if (!result) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Student is not deleted');
+    }
+    //transaction-2
+    const deleteUserResult = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, sesssion },
+    );
+    if (!deleteUserResult) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'User is not deleted');
+    }
+    await sesssion.commitTransaction();
+    await sesssion.endSession();
     return result;
-}
+  } catch (err) {
+    sesssion.abortTransaction();
+    throw new AppError(httpStatus.BAD_REQUEST,"Student is not deleted")
+  }
+};
 
-const getSingleStudentFromDB=async(id:string)=>{
-    const result=await StudentModel.findById(id);
-    // const result=await StudentModel.aggregate([
-    //     {$match:{id:id}}
-    // ])
-    return result;
-}
-
-
-const deleteStudentFromDB=async(id:string)=>{
-    const result=await StudentModel.updateOne({id:id} , {isDeleted:true});
-    return result;
-}
-
-export const StudentServices={
-   getAllStudentsFromDB,getSingleStudentFromDB,deleteStudentFromDB
-}
+export const StudentServices = {
+  getAllStudentsFromDB,
+  getSingleStudentFromDB,
+  deleteStudentFromDB,
+};
